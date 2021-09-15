bcff47c45cbae03e1173518ed0c670545cecc0b4
Andrew Osborn <osbornandrewj@gmail.com>
2021-09-15 13:05:19 -0700

    commit tree a671eaf577d91a45adf1d3a6e597580909075300

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
 // Code your solution in this file!
+const drivers = ['Sally', 'Bob', 'Freddy', 'Claudia']
+const returnFirstTwoDrivers = function(){
+    return drivers.splice(0,2);
+}
+
+const returnLastTwoDrivers = function() {
+    return drivers.splice(-2);
+}
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(x) {
+    return function fareMultiplier(){
+        let y = (x * 5)
+        return y
+    }
+}
+
+function fareDoubler(){
+    let x = 2;
+    return (createFareMultiplier(x) * 2);
+}
